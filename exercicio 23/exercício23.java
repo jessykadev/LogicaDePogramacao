bb921fef616e4bf838400e9fd63bb868812582f7
Jessyka Ázara <142222515+jessykadev@users.noreply.github.com>
2023-11-24 15:42:02 -0300

    exercício 23

---
+++ exercicio 23/exercício23.java
@@ -0,0 +1,14 @@
+class MeuPrimeiroProgramaJava {
+    public static void main(String[] args) {
+        int inteiro = 47;
+        // char caracter = 'F';
+        double real = 1.65;
+        String frase = "Jéssyka Azara";
+        boolean VF = true;
+
+        if (VF == true) {
+            System.out.println("Eu sou a " + frase + ", tenho " + inteiro +" anos e tenho" + real + "m de altura");
+     }
+     System.exit(0);
+    }
+}
