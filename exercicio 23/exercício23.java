ebd1951fbcf30298f93b9487c59fede7eff991d1
Jessyka Ázara <142222515+jessykadev@users.noreply.github.com>
2023-11-24 17:45:02 -0300

    exercício 24

--- exercicio 23/exercício23.java
+++ exercicio 23/exercício23.java
@@ -1,14 +1,15 @@
 class MeuPrimeiroProgramaJava {
     public static void main(String[] args) {
-        int inteiro = 47;
+        int inteiro = 26;
         // char caracter = 'F';
-        double real = 1.65;
+        double real = 1.60;
         String frase = "Jéssyka Azara";
         boolean VF = true;
 
         if (VF == true) {
-            System.out.println("Eu sou a " + frase + ", tenho " + inteiro +" anos e tenho" + real + "m de altura");
+            System.out.println("Eu sou a " + frase + ", tenho " + inteiro +" anos e tenho " + real + "m de altura");
      }
      System.exit(0);
     }
 }
+
---
+++ exercicio 24/exer24.java
@@ -0,0 +1,17 @@
+/**
+ * exer24
+ */
+public class exer24 {
+public static void main(String[] args) {
+    int idade = 47;
+    char genero = 'F';
+    double peso = 60;
+    String nome = "Maria";
+    String curso = "Ciência da Computação.";
+
+    System.out.println("Eu sou a " + nome + " tenho " + idade + " anos e tenho " + peso + "Kg de peso e curso " + curso);
+    System.exit(0);
+
+}
+    
+}
